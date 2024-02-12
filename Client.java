71b4f72e3f64a2633a71ec00ecd3cfd6f6b5f7a2
yataknemogy <124701682+yataknemogy@users.noreply.github.com>
2024-02-12 23:21:45 +0300

    Update Client.java
    remove two try and catch

--- Client.java
+++ Client.java
@@ -3,35 +3,29 @@ import java.net.Socket;
 
 public class Client {
 
-    private static Socket clientSocket;
-    private static BufferedReader reader;
-    private static BufferedReader in;
-    private static BufferedWriter out;
+    public static Socket clientSocket;
+    public static BufferedReader reader;
+    public static BufferedReader in;
+    public static BufferedWriter out;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         try {
-            try{
-                clientSocket = new Socket("localhost", 3345);
-                reader = new BufferedReader(new InputStreamReader(System.in));
-                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+            clientSocket = new Socket("localhost", 3345);
+            reader = new BufferedReader(new InputStreamReader(System.in));
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
 
-                System.out.println("Please, write in console:");
-                String word = reader.readLine();
-                out.write(word + "\n");
-                out.flush();
-                String serverWord = in.readLine();
-                System.out.println(serverWord);
-            } finally {
-                System.out.println("The client was closed");
-                clientSocket.close();
-                in.close();
-                out.close();
-            }
-        } catch (IOException e) {
-            System.err.println(e);
+            System.out.println("Please, write in console:");
+            String word = reader.readLine();
+            out.write(word + "\n");
+            out.flush();
+            String serverWord = in.readLine();
+            System.out.println(serverWord);
+        } finally {
+            System.out.println("The client was closed");
+            clientSocket.close();
+            in.close();
+            out.close();
         }
-
     }
 }
-
