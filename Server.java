faa1bcd26888a52eb424efafeba4fe6a5c957333
yataknemogy <124701682+yataknemogy@users.noreply.github.com>
2024-02-12 23:22:23 +0300

    Update Server.java
    remove two try and catch

--- Server.java
+++ Server.java
@@ -4,41 +4,28 @@ import java.net.Socket;
 
 public class Server {
 
-    private static Socket clientSocket;
-    private static ServerSocket server;
-    private static BufferedReader in;
-    private static BufferedWriter out;
+    public static Socket clientSocket;
+    public static ServerSocket server;
+    public static BufferedReader in;
+    public static BufferedWriter out;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         try {
-            try {
-                server = new ServerSocket(3345);
-                System.out.println("Server Start!");
-                clientSocket = server.accept();
-                try {
-                    in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                    // и отправлять
-                    out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+            server = new ServerSocket(3345);
+            System.out.println("Server Start!");
+            clientSocket = server.accept();
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            // и отправлять
+            out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
 
-                    String word = in.readLine();
-                    System.out.println(word);
-                    out.write("It server, you wrote " + word + "\n");
-                    out.flush();
-
-                }
-                finally {
-                    clientSocket.close();
-                    in.close();
-                    out.close();
-                }
-            }
-            finally {
-                System.out.println("Server closed!");
-                server.close();
-            }
-        }
-        catch (IOException e) {
-            System.err.println(e);
+            String word = in.readLine();
+            System.out.println(word);
+            out.write("It server, you wrote " + word + "\n");
+            out.flush();
+        } finally {
+            clientSocket.close();
+            in.close();
+            out.close();
         }
     }
 }
